c0b67cb29b8463be5848b7cf33ae36aea48be478
thnh203 <thanhtruongthe2k5@gmail.com>
2026-07-18 11:15:59 +0700

    Update

--- market-pipeline/models/__init__.py
+++
@@ -1,2 +0,0 @@
-# models/__init__.py
-from .schemas import CareerEntry, DbData, PineconeData
--- market-pipeline/processors/__init__.py
+++
@@ -1,3 +0,0 @@
-# processors/__init__.py
-from .pg_writer import upsert_career_to_pg, ensure_table_exists
-from .pinecone_uploader import upsert_to_pinecone
